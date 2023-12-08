1a305f2596cbb3020121b0a19ee891b0346bfda3
ori raisfeld <raisfeldori@gmail.com>
2023-12-08 19:45:34 +0200

    added a print line for the compilation

--- compile.py
+++ compile.py
@@ -9,7 +9,7 @@ from os import getcwd
 COMPILE = f"""
 {getcwd()}\\venv\\Scripts\\python -m PyInstaller --noconfirm --onefile --icon "{getcwd()}/raisfeld-encrypter-website/static/icon.ico" --name "Raisfeld encrypter" --add-data "{getcwd()}/venv/Lib/site-packages/fast_fs/__init__.py;." --add-data "{getcwd()}/venv/Lib/site-packages/fast_fs/fast_fs.cp312-win_amd64.pyd;." --add-data "{getcwd()}/venv/Lib/site-packages/fast_fs/__pycache__;__pycache__/" --hidden-import "fast fs"  "{getcwd()}/compile.py
 """""
-print("")
+print(f"command to used for compilation:\n{COMPILE}")
 
 if __name__ == "__main__":
     from src.main import main
